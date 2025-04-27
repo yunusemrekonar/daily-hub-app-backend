0f7d1805c46572c48147cd2d3ec11dc692f29d10
Emre <ynsemrknr@hotmail.com>
2025-04-27 19:59:18 +0300

    Set up authentication routes and implemented registration and login functionality

---
+++ routes/auth.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const router = express.Router();
+
+// Login
+router.post('/login', async (req,res) => {
+    const {email, password} = req.body;
+
+    const user = await User.findOne({email});
+
+    if (!user) {
+        return res.status(400).json({ message: 'Invalid credentials'});
+    }
+
+    // JWT token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {expiresIn: '1h'});
+
+    // If login is complated successfully, send to user with token
+    res.json({ message: 'Login successful', token});
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 // Import Packages
-const connectDB = require('./config/db');
 const express = require('express');
 const cors = require('cors');
 const User = require('./models/User');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/auth');
+const mongoose = require('mongoose');
+
 require('dotenv').config();
 
-// Connect to MongoDB
-connectDB();
 
 // Create the application
 const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
 // Endpoint for User Creation
-
 app.post('/api/users', async(req, res) => {
     const{ name, email, password } = req.body;
 
